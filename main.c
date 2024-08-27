afcb3785ae7f94581fa62bbe5b9b557fa6b016aa
midhun107107 <midhunmohan107@gmail.com>
2024-08-27 17:09:19 +0530

    added main.c

---
+++ main.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+//Login System: Users can access the system by entering their username and password
+//Ticket Purchase: Logged-in individuals may reserve tickets for available buses by entering the bus number, their name, and age.
+//The program allocates a seat number and decreases the number of available seats on the selected bus.
+//Cancel Tickets: By entering their name, users can cancel their tickets.
+//The program expands the number of available seats while removing the passenger entry.
+//Checking Bus Status: Users may check the status of the bus they are currently scheduled to ride on.
+//The program displays information such as the bus number, origin and destination, total number of seats, available seats, and fare.
+
+void compare_usrname_password(*char,*char);
+void create_account();
+void create_username_password(*char,*char);
+void login(*char,*char );
+void main()
+{
+
+printf("Test");
+
+}
+
+
+
+void login(*char username ,*char password )
+{
+
+
+
+
+}
