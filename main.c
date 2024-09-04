0c1d6c7371601e2185c6ae638c6d1f0e772c7a5f
midhun107107 <midhunmohan107@gmail.com>
2024-09-04 17:52:00 +0530

    added main.h ,modified main.c,added switch case

--- main.c
+++ main.c
@@ -1,7 +1,4 @@
-#include<stdio.h>
-#include"user.h"
-#include"Book_seat.h"
-#include"bus_layout.h"
+#include"main.h"
 //Login System: Users can access the system by entering their username and password
 //Ticket Purchase: Logged-in individuals may reserve tickets for available buses by entering the bus number, their name, and age.
 //The program allocates a seat number and decreases the number of available seats on the selected bus.
@@ -10,28 +7,43 @@
 //Checking Bus Status: Users may check the status of the bus they are currently scheduled to ride on.
 //The program displays information such as the bus number, origin and destination, total number of seats, available seats, and fare.
 
-void compare_usrname_password(char *,char *);
-void create_account();
-void create_username_password();
-void login(char *,char *);
-void bus_layout(int ,int);
-void select_seat(int);
+
 void main()
 {
+    char user_input;
+
+
 
     while(1)
     {
-        bus_layout(0,0);
-        add_new_user();
-        printf("debug\n");
+        user_input = user_input_data();
+
+        switch(user_input)
+        {
+            case '1': add_new_user();
+                      break;
+            case '2'://login();
+                      printf("test");
+                       break;
+            case '3': bus_layout(0,0);
+                      break;
+
+        }
     }
-printf("Test\n\n");
-create_username_password();
-bus_layout(0,0);
 
 }
 
 
+
+char user_input_data()
+{
+    char a;
+    printf("Enter the option \n");
+    printf("1 - New user\n2 - Login\n3 - Show bus layout\n");
+    scanf("%c",&a);
+
+    return a;
+}
 
 
 
---
+++ main.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include"user.h"
+#include"Book_seat.h"
+#include"bus_layout.h"
+
+char user_input_data();
+void compare_usrname_password(char *,char *);
+void create_account();
+void create_username_password();
+void login(char *,char *);
+void bus_layout(int ,int);
+void select_seat(int);
